fc80f3a5668f4174e37511424e263310ee190ecc
GiovanniRSantos <77495599+GiovanniRSantos@users.noreply.github.com>
2022-05-14 14:41:19 -0300

    refatorando nome da fila de eventos

--- src/main/java/tcc/bank/account/Configuration/EventPublicationConfiguration.java
+++ src/main/java/tcc/bank/account/Configuration/EventPublicationConfiguration.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class EventPublicationConfiguration {
-    @Value("${axon.amqp.exchange:banco.eventos}")
+    @Value("${axon.amqp.exchange:bank.events}")
     String exchangeName;
 
     @Bean
