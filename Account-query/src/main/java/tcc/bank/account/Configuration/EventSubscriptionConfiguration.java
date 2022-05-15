0cf8185bc85675c636975d4262c90e3d022090fd
GiovanniRSantos <77495599+GiovanniRSantos@users.noreply.github.com>
2022-05-15 11:53:40 -0300

    Corrigindo dependencias erradas do pom

--- Account-query/src/main/java/tcc/bank/account/Configuration/EventSubscriptionConfiguration.java
+++ Account-query/src/main/java/tcc/bank/account/Configuration/EventSubscriptionConfiguration.java
@@ -9,15 +9,16 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class EventSubscriptionConfiguration {
 
+    @Bean
     public SpringAMQPMessageSource complaintEventsMethod(Serializer serializer) {
         return new SpringAMQPMessageSource(new DefaultAMQPMessageConverter(serializer)) {
 
             @RabbitListener(queues = "${axon.amqp.exchange}")
             @Override
             public void onMessage(Message message, Channel channel) {
-                log.debug("Event Received: {}", message.getBody().toString());
                 super.onMessage(message, channel);
             }
         };
