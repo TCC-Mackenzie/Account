096e87f11ecdc444cdb7e555894c34738b9a95ff
GiovanniRSantos <77495599+GiovanniRSantos@users.noreply.github.com>
2022-05-15 16:54:07 -0300

    Consumindo service de conta para realizar a persistencia de dados

--- Account-command/src/main/java/tcc/bank/account/Controller/AccountController.java
+++ Account-command/src/main/java/tcc/bank/account/Controller/AccountController.java
@@ -5,22 +5,31 @@ import org.springframework.web.bind.annotation.*;
 import tcc.bank.account.Command.CreateAccountCommand;
 import tcc.bank.account.Command.UpdateBalanceCommand;
 import tcc.bank.account.DTO.AccountDTO;
+import tcc.bank.account.service.core.AccountService;
 
+import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 
 @RestController
-@RequestMapping(value = "")
 public class AccountController {
 
+    private final AccountService accountService;
     private CommandGateway commandGateway;
 
-    public AccountController(CommandGateway commandGateway) {
+    public AccountController(AccountService accountService, CommandGateway commandGateway) {
+        this.accountService = accountService;
         this.commandGateway = commandGateway;
     }
 
     @PostMapping(value = "/v1.0")
-    public CompletableFuture<String> create(@RequestBody AccountDTO dto) {
-        var command = new CreateAccountCommand(1, dto.getStatus());
+    public CompletableFuture<String> create() {
+        var gerador = new Random();
+        var accountId = gerador.nextInt(999999);
+        while(accountService.findAccountByAccountId(accountId) != null) {
+            accountId = gerador.nextInt(999999);
+        }
+        var account = accountService.create(accountId);
+        var command = new CreateAccountCommand(account.getAccountId(), account.getStatus().getDescription());
         return commandGateway.send(command);
     }
 
